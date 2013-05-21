a19d72a9aed8f492d5ffefeea74fe4461861520f
mh <mh@immerda.ch>
2013-05-21 22:59:45 +0200

    debian init script does not have a status command

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -1,8 +1,14 @@
+# debian specific things
 class iodine::server::debian inherits iodine::server::base {
+  Service['iodined']{
+    hasstatus => false,
+  }
   file{'/etc/default/iodine':
     content => template('iodine/default.erb'),
     require => Package['iodine'],
-    notify => Service['iodined'],
-    owner => root, group => 0, mode => 0600;
+    notify  => Service['iodined'],
+    owner   => root,
+    group   => 0,
+    mode    => '0600';
   }
 }
