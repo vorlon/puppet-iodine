6550218e7b4c3ad71a350849b188e2af2a0d3fb7
Kyle Anderson <kyle@xkyle.com>
2013-05-29 21:28:15 -0700

    client params with openwrt

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,10 +2,24 @@ class iodine::params {
  $server_package = $::operatingsystem ? {
     /RedHat|CentOS/ => 'iodine',
     /Debian|Ubuntu/ => 'iodine',
+    'OpenWrt'       => 'iodined',
   }
  
  $server_service = $::operatingsystem ? {
     /RedHat|CentOS/ => 'iodine-server',
     /Debian|Ubuntu/ => 'iodined',
+    'OpenWrt'       => 'iodined',
+  }
+ 
+ $client_package = $::operatingsystem ? {
+    /RedHat|CentOS/ => 'iodine',
+    /Debian|Ubuntu/ => 'iodine',
+    'OpenWrt'       => 'iodine',
+  }
+ 
+ $client_service = $::operatingsystem ? {
+    /RedHat|CentOS/ => 'iodine-client',
+    /Debian|Ubuntu/ => undef,
+    'OpenWrt'       => undef,
   }
 }
