975ca51610bd089d98640fa132534d5a06496bf7
Kyle Anderson <kyle@xkyle.com>
2014-03-02 10:42:59 -0800

    Fix specs to not use hiera

--- spec/classes/iodine_server_spec.rb
+++ spec/classes/iodine_server_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'iodine::server', :type => :class do
-  let(:hiera_data) {{ 'iodine::password' => 'example_test_password' }}
 
   ['Debian', 'RedHat'].each do |system|
     let(:facts) {{ :osfamily => system }}
+    let(:params) {{ 'password' => 'example_test_password' }}
+    
     it { should contain_class 'iodine::server::install' }
   end
   
